7b58b1f9e9aca24935c1e5e78c0163192ccd7f14
giuseppe Varas <giuseppe.varas@efby.cl>
2017-10-29 00:15:32 -0300

    uso de ngIf y ngFor

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-body',
     templateUrl: 'body.component.html'
   })
   export class BodyComponent {
+     mostrarCard:boolean=false;
+     public frase:any={
+          mensaje:"Parece que no pero las guapas también se tiran pedos, también los listos sumamos con los dedos",
+          autor:"Doble V"
+      }
+      personajes=["spiderman","Venom","Dr. Octupus"];
     
   }
   
